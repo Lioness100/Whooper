3a4bf7fdb67fd8f4ca854c1ca5ca6c97a11fe502
Lioness100 <jchickmm2@gmail.com>
2020-08-22 16:23:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js);
+const client = new Discord.Client();
+
+client.on('ready', (ready) => {
+   console.log('why revolug. why.')
+};
+
+const woopers = ['insert link here', 'and here', 'and here', 'and maybe here as well if you are a true madlad']
+
+client.on('message', (message) => {
+   if (message.content === !wooper)
+      message.channel.send(new Discord.MessageEmbed()
+         .setColor('GREEN')
+         .setTitle('revolug why')
+         .setImage(woopers[Math.floor(Math.random() * woopers.length)]);
+      );
+}
+
+client.login('put your token here');
